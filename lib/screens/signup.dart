6e0d15b60455999b03457548a7f970d0c5507080
sonysn <stephen.nyamali@gmail.com>
2023-04-26 18:37:07 +0100

    login page fix

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '/colorscheme.dart';
 import '/screens/bottom_nav.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:path/path.dart' as path;
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -17,6 +22,145 @@ class _SignUpState extends State<SignUp> {
   TextEditingController password = TextEditingController();
   TextEditingController email = TextEditingController();
   TextEditingController homeAddress = TextEditingController();
+
+
+  //PICK PROFILE IMAGE
+  File? _image;
+  final picker = ImagePicker();
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  //PICK DOCUMENTS
+  File? _licenseFile;
+  File? _permitFile;
+
+  Future<void> _pickLicenseFile() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf', 'jpg', 'jpeg', 'png'],
+      );
+
+      if (result != null) {
+        setState(() {
+          _licenseFile = File(result.files.single.path!);
+        });
+      }
+    } on PlatformException catch (e) {
+      print("Error picking file: $e");
+    }
+  }
+
+  Future<void> _pickPermitFile() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf', 'jpg', 'jpeg', 'png'],
+      );
+
+      if (result != null) {
+        setState(() {
+          _permitFile = File(result.files.single.path!);
+        });
+      }
+    } on PlatformException catch (e) {
+      print("Error picking file: $e");
+    }
+  }
+
+  //THEIR WIDGETS
+  Widget _buildSelectedLicenseFile() {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(Icons.attach_file),
+          SizedBox(width: 20.0),
+          Expanded(
+            child: Text(
+              _licenseFile != null ? path.basename(_licenseFile!.path) : 'No file selected',
+              style: TextStyle(fontSize: 16.0),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _licenseFile = null;
+              });
+            },
+            child: Icon(Icons.close),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSelectedPermitFile() {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(Icons.attach_file),
+          SizedBox(width: 20.0),
+          Expanded(
+            child: Text(
+              _permitFile != null ? path.basename(_permitFile!.path) : 'No file selected',
+              style: TextStyle(fontSize: 16.0),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _permitFile = null;
+              });
+            },
+            child: Icon(Icons.close),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Widget _buildSelectLicenseFileButton() {
+  //   return ElevatedButton.icon(
+  //     onPressed: _pickLicenseFile,
+  //     icon: const Icon(Icons.attach_file),
+  //     label: const Text('Select driver\'s license'),
+  //   );
+  // }
+
+  // Widget _buildSelectPermitFileButton() {
+  //   return ElevatedButton.icon(
+  //     onPressed: _pickPermitFile,
+  //     icon: const Icon(Icons.attach_file),
+  //     label: const Text('Select Vehicle\'s Permit'),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +181,46 @@ class _SignUpState extends State<SignUp> {
                       fontWeight: FontWeight.bold,
                       fontSize: 27,
                       color: Colors.black),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 12),
+                child: Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          _image != null ? FileImage(_image!) : null,
+                      child: _image == null
+                          ? const Icon(
+                              Icons.person,
+                              size: 30,
+                            )
+                          : null,
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                        onPressed: getImageFromCamera,
+                        style: ElevatedButton.styleFrom(
+                            primary: customPurple,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(20),
+                                bottom: Radius.circular(20),
+                              ),
+                            )),
+                        child: const Text('Take a picture')),
+                  ],
                 ),
               ),
               const Padding(
@@ -231,32 +415,95 @@ class _SignUpState extends State<SignUp> {
                 ),
               ),
               Container(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    children: [
-                      //TextField for name
-                      TextField(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: TextFormField(
                         controller: homeAddress,
                         cursorColor: customPurple,
-                        keyboardType: TextInputType.text,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 3, // set maxLines to 2 for a double-line input
                         decoration: const InputDecoration(
-                            hintText: 'Enter your Address',
-                            contentPadding: EdgeInsets.all(15),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              borderSide: BorderSide(color: Colors.black),
-                            ),
-                            filled: true,
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              borderSide: BorderSide(color: Colors.yellow),
-                            )),
+                          hintText: 'Enter your Address',
+                          filled: true,
+                          contentPadding: EdgeInsets.all(15),
+                          border: InputBorder.none,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your home address';
+                          }
+                          return null;
+                        },
                       ),
-                    ],
-                  )),
+                    ),
+                  ],
+                ),
+              ),
+
               const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.only(left: 12),
+                child: Text(
+                  'Driver\'s License',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(12),
+                child: GestureDetector(
+                  onTap: (){
+                    _pickLicenseFile();
+                  },
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        _pickLicenseFile != null ? _buildSelectedLicenseFile() : Container(),
+                        const SizedBox(height: 20),
+                        //_buildSelectLicenseFileButton(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.only(left: 12),
+                child: Text(
+                  'Vehicle\'s Permit',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(12),
+                child: GestureDetector(
+                  onTap: () {
+                    _pickPermitFile();
+                  },
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        _pickPermitFile != null ? _buildSelectedPermitFile() : Container(),
+                        const SizedBox(height: 20),
+                        //_buildSelectPermitFileButton(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               //sign up button
               Center(
                 child: ElevatedButton(
